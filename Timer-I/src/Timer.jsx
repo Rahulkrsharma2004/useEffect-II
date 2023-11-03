c2910cc4df6a46473f5d387427c4f988076188fc
RahulKrSharma2004 <kumarrahulbasdiha@gmail.com>
2023-11-04 02:39:07 +0530

    Added a new assignment of Timer-I

---
+++ Timer-I/src/Timer.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Timer: {count} </h1>
+    </div>
+  );
+}
+
+export default Timer;
---
+++ Timer-I/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Timer from './Timer.jsx'
+import './index.css'
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Timer />
+  </React.StrictMode>,
+)
